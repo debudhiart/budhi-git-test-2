b5b61329523749b03f06b349a0a5eb70836a734c
debudhiart <debudhiart@gmail.com>
2021-02-14 01:50:01 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta similique a hic ipsa odit perferendis vitae enim! Omnis adipisci consequuntur minima quibusdam alias accusantium, blanditiis, architecto est, vel dicta incidunt?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
